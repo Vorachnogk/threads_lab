f11eee539e83b5a07e6988338baaae223fcfda42
Vorachnogk <kirill.goncharov16.11.1999@gmail.com>
2020-03-03 23:45:23 +0200

    third lab first task Locker

--- src/BankTest.java
+++ src/BankTest.java
@@ -1,14 +1,21 @@
-public class UnsynchBankTest {
+// import java.util.concurrent.ExecutorService;
+// import java.util.concurrent.Executors;
+
+public class BankTest {
+
     public static final int NACCOUNTS = 10;
     public static final int INITIAL_BALANCE = 10000;
 
     public static void main(String[] args) {
+
         // Створити банк з 10 рахунками та балансом 10000
         Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
-        for (int i = 0; i < NACCOUNTS; i++) {
+        // ExecutorService executor = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < NACCOUNTS; i++){
             TransferThread t = new TransferThread(b, i, INITIAL_BALANCE);
             t.setPriority(Thread.NORM_PRIORITY + i % 2);
-            t.start();
+            // executor.submit(t);
+            t.start () ;
         }
     }
 }
