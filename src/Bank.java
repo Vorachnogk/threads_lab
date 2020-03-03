7a560c779a979d39d884955ce4222ee2cfc5992f
Vorachnogk <kirill.goncharov16.11.1999@gmail.com>
2020-03-03 23:34:47 +0200

    third lab first task Atomic

--- src/Bank.java
+++ src/Bank.java
@@ -1,35 +1,35 @@
-public class Bank {
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicLong;
 
+public class Bank {
     public static final int NTEST = 10000;
-    private final int[] accounts;               // масив рахунків
-    private long ntransacts = 0;
+    private final AtomicIntegerArray accounts;               // масив рахунків
+    private AtomicLong ntransacts = new AtomicLong();
 
     public Bank(int n, int initialBalance) {
-        accounts = new int[n];                  // створити рахунок в банку
+        accounts = new AtomicIntegerArray(n);                  // створити рахунок в банку
         int i;
-        for (i = 0; i < accounts.length; i++)   // нарахувати на кожний рахунок 10 000
-            accounts[i] = initialBalance;
-        ntransacts = 0;
-
+        for (i = 0; i < accounts.length(); i++)   // нарахувати на кожний рахунок 10 000
+            accounts.set(i, initialBalance);
+        ntransacts.set(0);
     }
 
-    public synchronized void transfer(int from, int to, int amount) throws InterruptedException {
-        accounts[from] -= amount;
-        accounts[to] += amount;
-        ntransacts++;
-        if (ntransacts % NTEST == 0) test();
+    public void transfer(int from, int to, int amount) throws InterruptedException {
+        accounts.getAndAdd(from, amount);
+        accounts.getAndAdd(to, -amount);
+        ntransacts.set(ntransacts.get() + 1);
+        if (ntransacts.get() % NTEST == 0) test();
     }
 
     public void test() {
         int sum = 0;
-        for (int i = 0; i < accounts.length; i++)
-            sum += accounts[i];
+        for (int i = 0; i < accounts.length(); i++)
+            sum += accounts.get(i);
         System.out.println("Transactions: " + ntransacts
                 + " Sum: " + sum);
-
     }
 
     public int size() {
-        return accounts.length;
+        return accounts.length();
     }
 }
